a2d23aace3a54c50d66a52dd3e62a3341652c144
MCodrescu <m.codrescu@outlook.com>
2022-01-07 22:35:27 -0600

    Adding Blog Page Edits

---
+++ navbar.js
@@ -0,0 +1,35 @@
+let navbar_html = `
+  <div class="container-fluid">
+    <a class="navbar-brand">Marcus Codrescu</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.html">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="blog-home.html">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Resume</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+`;
+
+let navbar = document.querySelector("#navbar");
+navbar.innerHTML = navbar_html;
