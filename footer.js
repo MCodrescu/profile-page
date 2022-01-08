87ebdb825f7befee053f810cf4ece71b555d0411
MCodrescu <m.codrescu@outlook.com>
2022-01-08 15:14:20 -0600

    Changes to Footer

--- footer.js
+++ footer.js
@@ -1,9 +1,10 @@
 let footer_data = `
     <div class="container-fluid bg-light">
-      <a href="index.html" class="text-muted">Home</a>
-      <a href="blog-home.html" class="text-muted">Blog</a>
-      <a href="#" class="text-muted">Resume</a>
-      <a href="contact.html" class="text-muted">Contact</a>
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active text-muted" href="#navbar">Back To Top</a>
+            </li>
+        </ul>
     </div>
 `;
 
