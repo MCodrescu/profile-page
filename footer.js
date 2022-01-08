b183ff91c439d2e09194ad124ef2f5f4dac72445
MCodrescu <m.codrescu@outlook.com>
2022-01-08 15:08:14 -0600

    Create a Footer

---
+++ footer.js
@@ -0,0 +1,11 @@
+let footer_data = `
+    <div class="container-fluid bg-light">
+      <a href="index.html" class="text-muted">Home</a>
+      <a href="blog-home.html" class="text-muted">Blog</a>
+      <a href="#" class="text-muted">Resume</a>
+      <a href="contact.html" class="text-muted">Contact</a>
+    </div>
+`;
+
+let footer = document.querySelector("#footer");
+footer.innerHTML = footer_data;
